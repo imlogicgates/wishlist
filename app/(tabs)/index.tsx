fcc61a8732f0ae52302d8a9e585886acff79ef9b
Jig James Marababol <jjbmarababol@gmail.com>
2024-12-08 14:53:59 +0800

    [WL-3] Wishlist component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,13 @@
-export default function TabOneScreen() {
-  return <></>;
-}
+import { View } from "@/components/Themed";
+import WishlistList from "@/components/WishlistList";
+import React from "react";
+
+const TabOneScreen: React.FC = () => {
+  return (
+    <View className="flex-1 bg-white dark:bg-gray-900">
+      <WishlistList />
+    </View>
+  );
+};
+
+export default TabOneScreen;
