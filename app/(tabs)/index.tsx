ed44ac6359d9d5a3ceac5092437c6163a36f1df1
Jig James Marababol <jjbmarababol@gmail.com>
2024-12-08 14:11:24 +0800

    [WL-2] Fix font implementation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "@/components/Themed";
 export default function TabOneScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold">Tab One</Text>
+      <Text className="text-2xl font-bold font-samsung-sharp">Tab One</Text>
       <View
         className="h-1 w-80 bg-gray-500"
         lightColor="#eee"
